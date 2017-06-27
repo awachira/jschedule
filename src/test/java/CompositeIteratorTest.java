cd45e67e8ef6c9f7ac00ce325ca6f303307d0f4d
awachira <andrewachira@gmail.com>
2017-06-27 20:44:15 +0300

    Corrected CW alignment.

--- src/test/java/CompositeIteratorTest.java
+++ src/test/java/CompositeIteratorTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2008 Tom White (tom at tiling dot org) Copyright 2009 - Andrew Wachira (andrewachira at
- * gmail dot com)
+ * Copyright 2008 - 2009 Tom White (tom at tiling dot org) 
+ * Copyright 2009 -      Andrew Wachira (andrewachira at gmail dot com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
